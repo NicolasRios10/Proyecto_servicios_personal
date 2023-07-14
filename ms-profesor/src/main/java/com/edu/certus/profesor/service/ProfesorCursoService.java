84d37f38c00a58b5d2d6a39a9fdd1eb7f12c4373
NicolasRios10 <72674272@certus.edu.pe>
2023-07-13 21:15:47 -0500

    V.6 proyecto terminado con la implementacion de docker

---
+++ ms-profesor/src/main/java/com/edu/certus/profesor/service/ProfesorCursoService.java
@@ -0,0 +1,14 @@
+package com.edu.certus.profesor.service;
+
+import com.edu.certus.profesor.dto.ProfesorCursoDto;
+import com.edu.certus.profesor.dto.ResponseDto;
+
+public interface ProfesorCursoService {
+	public ResponseDto getAllProfesorCurso();
+	public ResponseDto getProfesorCurso(Long idProfesor);
+	public ResponseDto createProfesorCurso(ProfesorCursoDto profesorCursoDto);
+	public ResponseDto updateProfesorCurso(ProfesorCursoDto profesorCursoDto);
+
+	public ResponseDto getCursoProfesor(Long idCurso);
+	public ResponseDto deleteProfesorCurso(Long id);
+}
